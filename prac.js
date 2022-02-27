8d716698aa6aea89382e562886b91fbe83ad05f5
Sujan Baskota <pokemon71118@gmail.com>
2022-02-27 19:33:32 +0545

    a little practice

--- prac.js
+++ prac.js
@@ -1,8 +1,12 @@
+//document object module(DOM)
 let element = document;
 function myFunc(){
     element.getElementById("para").innerHTML="<b> pikachu?? </b>";
+    //element=element.className
+   //element.style.color="brown";
+   //console.log(element.innerHTML)
 }
 console.log(myFunc());
 
-let a = window;
-console.log(a);
+
+
