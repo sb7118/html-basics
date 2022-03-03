e282359791e775798ef2f2def17c5f88e475a333
Sujan Baskota <pokemon71118@gmail.com>
2022-03-03 21:41:01 +0545

    lil' bit

---
+++ web page/prac.js
@@ -0,0 +1,16 @@
+
+let pika = document;
+function myFunc(){
+    pika.getElementById("para").innerHTML="<b> pikachu?? </b>";
+}
+console.log(myFunc());
+
+
+
+let qwe = document.getElementById("gold").addEventListener("click" , function (e){
+    alert("this is heading");
+});
+
+
+
+
