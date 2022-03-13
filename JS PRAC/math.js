c467e38ea22f75ccc4f7691a7845952c3016665b
sb7118 <100357900+sb7118@users.noreply.github.com>
2022-03-13 08:32:01 +0545

    Add files via upload
    math objects

---
+++ JS PRAC/math.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+let x= 3;
+let y = 9;
+let z = x+y;
+
+//further maths objects
+z = Math.PI;
+z = Math.ceil(5.5);// 6++=
+z = Math.floor(5.5);//5
+//it will generate not more than 100 and not less than 50
+M = Math.floor( Math.random()*(100 - 50 /*not more than 100  */))+50/*not less than 50*/;// random number generator : *(max - min))+min
+z = Math.abs(-5.5);// returns  +ve
+z = Math.sqrt(25);// square root
+z = Math.pow(2 , 3);//(2**3)
+z = Math.min(1,234,345,5,-7);//returms minimum value
+z = Math.max(1,234,345,5,-7);//returms maximum value
+console.log(M);
+
+
